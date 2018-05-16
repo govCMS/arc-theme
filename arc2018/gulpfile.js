acff6737563b5056d92347517cc48c1fcbf6f408
Joseph Zhao <pandastixstudio@gmail.com>
2018-05-16 23:04:54 +1000

    Gulpfile.js placeholder

---
+++ arc2018/gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// *************************
+//
+// Run 'gulp' to watch directory for changes for images, fonts icons, Sass, etc.
+// Or for full site testing run 'gulp test'
+//
+// *************************
+
+
+// Include gulp.
+const gulp = require('gulp');
+
+// Default gulp task.
+gulp.task('default', []);
